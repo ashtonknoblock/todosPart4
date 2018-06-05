6261b404096f36844cb03331e205ba77b24f7ebe
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-06-05 13:10:44 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -16,20 +16,20 @@ class App extends Component {
 
   handleChecked = (event) => {
     const copy2 = this.state.todos.slice();
-    const thingToChange = copy2[event.target.id] 
+    const thingToChange = copy2[event.target.id]
     thingToChange.completed ? thingToChange.completed = false : thingToChange.completed = true;
-    this.setState({todos: copy2})
-    }
+    this.setState({ todos: copy2 })
+  }
 
-    destroyOnClick = (event) => {
-      const copy3 = this.state.todos.slice(); 
-      const Myindex = copy3.findIndex(item => item.id == event.target.id)+1
-      console.log(event.target.id)
-      console.log(Myindex)
-      copy3.splice(Myindex, 1) 
-      console.log(copy3)
-      this.setState({todos: copy3})
-    }
+  destroyOnClick = (event) => {
+    const copy3 = this.state.todos.slice();
+    const Myindex = copy3.findIndex(item => item.id == event.target.id) + 1
+    console.log(event.target.id)
+    console.log(Myindex)
+    copy3.splice(Myindex, 1)
+    console.log(copy3)
+    this.setState({ todos: copy3 })
+  }
 
 
   handleChange = (event) => {
@@ -39,25 +39,25 @@ class App extends Component {
 
   deleteCompleted = (event) => {
     const copy4 = this.state.todos.slice();
-    const toDeltete = copy4.filter(item => item.completed !== true )
-    this.setState({todos: toDeltete})
+    const toDeltete = copy4.filter(item => item.completed !== true)
+    this.setState({ todos: toDeltete })
 
   }
 
 
   handleKeyPress = (event) => {
     const copy = this.state.todos.slice();
-    const nextid = Number(copy[copy.length-1].id);
+    const nextid = Number(copy[copy.length - 1].id);
 
     const addedToDo = {
       "userId": 1,
-      "id": nextid+1,
+      "id": nextid + 1,
       "title": `${event.target.value}`,
       "completed": false
     };
     copy.push(addedToDo);
     if (event.key === "Enter") {
-      this.setState({todos: copy})
+      this.setState({ todos: copy })
       event.preventDefault();
       document.getElementById('text').value = "";
     }
@@ -69,15 +69,15 @@ class App extends Component {
       <section className="todoapp">
         <header className="header">
           <h1>To Do</h1>
-            <input type="text" id="text" className="new-todo" placeholder="What needs to be done?" 
+          <input type="text" id="text" className="new-todo" placeholder="What needs to be done?"
             onKeyPress={this.handleKeyPress} onChange={this.handleChange} autoFocus></input>
-            <TodoList todos={this.state.todos} onClick={(event) => this.handleChecked(event)} 
-            destroyOnClick={ (event) => this.destroyOnClick(event)}/>
+          <TodoList todos={this.state.todos} onClick={(event) => this.handleChecked(event)}
+            destroyOnClick={(event) => this.destroyOnClick(event)} />
         </header>
         <footer className="footer">
-         <span className="todo-count"><strong>0</strong> item(s) left</span>
-         <button className="clear-completed" onClick={this.deleteCompleted}>Clear completed</button>
-       </footer>
+          <span className="todo-count"><strong>0</strong> item(s) left</span>
+          <button className="clear-completed" onClick={this.deleteCompleted}>Clear completed</button>
+        </footer>
       </section>
     );
   }
@@ -87,23 +87,23 @@ class App extends Component {
 
 class TodoList extends Component {
 
-render() {
-  return(
-  <React.Fragment>
-    <section className="main" >
-      <ul className="todo-list">
-        {this.props.todos.map( todo => <TodoItem 
-        onClick={(event) => this.props.onClick(event)} 
-        destroyOnClick={(event) => this.props.destroyOnClick(event)}
-        TodoItem={todo.title} 
-        TodoCompleted={todo.completed} 
-        key={todo.id} 
-        id={todo.id} />)}
-      </ul>
-    </section>
-  </React.Fragment>
-  )
-}
+  render() {
+    return (
+      <React.Fragment>
+        <section className="main" >
+          <ul className="todo-list">
+            {this.props.todos.map(todo => <TodoItem
+              onClick={(event) => this.props.onClick(event)}
+              destroyOnClick={(event) => this.props.destroyOnClick(event)}
+              TodoItem={todo.title}
+              TodoCompleted={todo.completed}
+              key={todo.id}
+              id={todo.id} />)}
+          </ul>
+        </section>
+      </React.Fragment>
+    )
+  }
 }
 
 
@@ -111,25 +111,25 @@ render() {
 class TodoItem extends Component {
 
   render() {
-  const isCompleted = this.props.TodoCompleted;
-  const renderComplete = isCompleted ? (
-    <input id={this.props.id-1} className="toggle" type="checkbox" onClick={(event) => this.props.onClick(event)} defaultChecked></input> //if completed or true, then its checked.
-  ) : (
-    <input id={this.props.id-1} className="toggle" type="checkbox" onClick={(event) => this.props.onClick(event)} ></input> //if not complete, or false, then its not checked.
-  )
-  
-  
-    return(
-  <li className={isCompleted ? 'completed' : ''}>
-      <div className="view">
-        {renderComplete} 
-        <label>{this.props.TodoItem}</label>
-        <button className="destroy" id={this.props.id-1} onClick={(event) => this.props.destroyOnClick(event)}></button>
-      </div>
-  </li>
+    const isCompleted = this.props.TodoCompleted;
+    const renderComplete = isCompleted ? (
+      <input id={this.props.id - 1} className="toggle" type="checkbox" onClick={(event) => this.props.onClick(event)} defaultChecked></input> //if completed or true, then its checked.
+    ) : (
+        <input id={this.props.id - 1} className="toggle" type="checkbox" onClick={(event) => this.props.onClick(event)} ></input> //if not complete, or false, then its not checked.
       )
-    }
+
+
+    return (
+      <li className={isCompleted ? 'completed' : ''}>
+        <div className="view">
+          {renderComplete}
+          <label>{this.props.TodoItem}</label>
+          <button className="destroy" id={this.props.id - 1} onClick={(event) => this.props.destroyOnClick(event)}></button>
+        </div>
+      </li>
+    )
   }
+}
 
 
 export default App;
